2bc6dbe894281f17246bc2b7a4c8e0037fe69fcb
sahyadri551 <sahyadri2413082@akgec.ac.in>
2025-09-19 19:22:50 +0530

    update particle and explode particle function is added

--- script.js
+++ script.js
@@ -11,13 +11,41 @@ function colors(){
     return `rgb(${r},${g},${b})`;
 }
 
-function draw(particle) {
+function draw(p) {
     ctx.save();
-    ctx.globalAlpha = particle.op;
-    ctx.fillStyle = particle.color;
+    ctx.globalAlpha = p.op;
+    ctx.fillStyle = p.color;
     ctx.beginPath();
-    ctx.arc(particle.x, particle.y, particle.size, 0, Math.PI * 2);
+    ctx.arc(p.x, p.y, p.size, 0, Math.PI * 2);
     ctx.fill();
     ctx.restore();
+}
+
+function updateParticle(p) {
+    p.x += p.xsp;
+    p.y += p.ysp;
+    if (p.alpha > p.ch) {
+        p.op -= p.ch;
+    } else {
+        p.op = 0;
+    }
+}
+
+function explode(e) {
+    const x = e.clientX;
+    const y = e.clientY;
+
+    for (let i = 0; i < n; i++){
+        arr.push({
+        x: x,
+        y: y,
+        size: Math.random() * 5 + 1,
+        xsp: Math.random() * 4 - 2,
+        ysp: Math.random() * 4 - 2,
+        color: colors(),
+        op: 1,
+        ch: 0.02,
+        });
+    }
 }
 
